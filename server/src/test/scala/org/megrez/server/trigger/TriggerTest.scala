510779f14e1e1c98a07d9b3bf83abc2868342a4e
nana <nana@nana-laptop.(none)>
2010-10-26 18:11:21 +0800

    nana:add test for Trigger

--- server/src/test/scala/org/megrez/server/trigger/TriggerTest.scala
+++ server/src/test/scala/org/megrez/server/trigger/TriggerTest.scala
@@ -1,12 +1,35 @@
 package scala.org.megrez.server.trigger
 
+import scala.actors._
+import Actor._
+import main.scala.org.megrez.server.trigger.VersionControl
 import org.scalatest.Spec
-import org.megrez.server.trigger.Trigger;
+import org.megrez.server.trigger.Trigger
 import org.scalatest._
 import org.scalatest.matchers._
+import org.megrez.server._
 
 class TriggerTest extends Spec with ShouldMatchers with BeforeAndAfterEach {
-    describe("shoud get latest revision number"){
-
+  describe("shoud call versionControl methods") {
+    it("when build revision is setting") {
+      val versionControl: VersionControlMocker = new VersionControlMocker(new Pipeline("name", "url", "revision"))
+      val scheduler = self
+      val trigger: Trigger = new Trigger(versionControl,scheduler)
+      trigger.start
+      trigger ! "haha"
+      receive{
+        case msg:TriggerMessage =>println("success")
+      }
     }
+  }
+}
+
+class VersionControlMocker(val pipeline: Pipeline) extends VersionControl {
+  def checkChange() = {if (pipeline.buildRevision != "") needTriggerScheduler = true}
+
+  def getChange(): TriggerMessage = {
+    if (needTriggerScheduler)
+      return new TriggerMessage(pipeline.name, pipeline.buildRevision)
+    return null
+  }
 }
