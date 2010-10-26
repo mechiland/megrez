b02a177f209606cb8d9a105d2a806dc7aecad4bd
whimet <whimet@gmail.com>
2010-10-26 18:47:04 +0800

    use Configuration to determine a pipeline's stage info

--- server/src/main/scala/org/megrez/server/Pipeline.scala
+++ server/src/main/scala/org/megrez/server/Pipeline.scala
@@ -2,6 +2,10 @@ package org.megrez.server
 
 import java.util.Calendar
 
+class Material(val url: String)
+class SvnMaterial(override val url: String) extends Material(url)
+class GitMaterial(override val url: String) extends Material(url)
+
 class Pipeline(val name:String, val repositoryUrl:String, val buildRevision:String,val buildDate:Calendar=null,val workDir:String ="")
 
 class Job(val name : String, val resources : Set[String], val tasks : List[Task])
@@ -11,6 +15,9 @@ class Task
 object Configuration {
   def hasNextStage(pipeline: String, stage: String) = {
     true
+  }
+  def firstStage(pipeline: String) = {
+    "stage1"
   }
   def nextStage(pipeline: String, stage: String) = {
     "stage2"
