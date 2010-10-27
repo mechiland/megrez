8fc58670e8dfad1dc0419556156cd8ec2b869c6c
vincentx <vincent.hao.xu@gmail.com>
2010-10-27 10:28:29 +0800

    change pattern to be string

--- server/src/main/scala/org/megrez/server/http/Route.scala
+++ server/src/main/scala/org/megrez/server/http/Route.scala
@@ -1,31 +1,35 @@
 package org.megrez.server.http
 
+import org.jboss.netty.handler.codec.http.HttpRequest
+
 trait Route {
-  def isMatch(request : org.jboss.netty.handler.codec.http.HttpRequest) : Boolean
-  def getHandler : Any
-  def matchedMethod(request : org.jboss.netty.handler.codec.http.HttpRequest) : Method
+  def isMatch(request: HttpRequest): Boolean
+
+  def getHandler: Any
+
+  def matchedMethod(request: org.jboss.netty.handler.codec.http.HttpRequest): Method
 }
 
-case class Connect(val pattern : Any, val methods : Set[Method], val handler : Any) extends Route {
-  override def isMatch(request : org.jboss.netty.handler.codec.http.HttpRequest) : Boolean = {
-    (pattern equals request.getUri) && (matchedMethod(request) != None)
+case class Connect(val pattern: String, val methods: Set[Method], val handler: Any) extends Route {
+  override def isMatch(request: HttpRequest): Boolean = {
+    (request.getUri.matches(pattern)) && (matchedMethod(request) != None)
   }
 
-  override def matchedMethod(request : org.jboss.netty.handler.codec.http.HttpRequest) : Method = {
-	methods.find(_.toString.equals(request.getMethod().getName())).get
+  override def matchedMethod(request: org.jboss.netty.handler.codec.http.HttpRequest): Method = {
+    methods.find(_.toString.equals(request.getMethod().getName())).get
   }
 
-  override def getHandler = handler 
+  override def getHandler = handler
 }
 
 object Route {
-  class Path(val pattern : Any, val methods : Set[Method]) {
-    def ->(handler : Any) = Connect(pattern, methods, handler)
+  class Path(val pattern: String, val methods: Set[Method]) {
+    def ->(handler: Any) = Connect(pattern, methods, handler)
   }
 
-  def path(path : String) = new Path(path, Set(GET, PUT, DELETE, POST))
+  def path(path: String) = new Path(path, Set(GET, PUT, DELETE, POST))
 
-  def get(path : String) = new Path(path, Set(GET))
+  def get(path: String) = new Path(path, Set(GET))
 
   def post(path: String) = new Path(path, Set(POST))
 
