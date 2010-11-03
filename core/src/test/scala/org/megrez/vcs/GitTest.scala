4c36d78f8b053a544fb3d50e6dc752510ced61e4
nana <nana@nana-laptop.(none)>
2010-11-03 19:09:41 +0800

    'added_new_file'

--- core/src/test/scala/org/megrez/vcs/GitTest.scala
+++ core/src/test/scala/org/megrez/vcs/GitTest.scala
@@ -9,35 +9,36 @@ class GitTest extends Spec with ShouldMatchers with BeforeAndAfterEach with Shel
   describe("Subversion") {
     it("should detect change if working dir empty") {
       val git = new Git(repositoryURL)
-       makeNewCommit(repositoryURL)
+      git.checkout(workingDir, None)
+      makeNewCommit(workingDir.getAbsolutePath)
       git.changes(workingDir) match {
-        case Some(revision : String) =>
+        case Some(revision: String) =>
         case _ => fail
       }
     }
 
-//    it("should return false if given dir not a directory") {
-//      val git = new Git(repositoryURL)
-//      git.isRepository(workingDir) should equal(false)
-//    }
-//
-//    it("should return true if given dir is a directory") {
-//      val git = new Git(repositoryURL)
-//      git.checkout(workingDir, None)
-//      git.isRepository(workingDir) should equal(true)
-//    }
+    //    it("should return false if given dir not a directory") {
+    //      val git = new Git(repositoryURL)
+    //      git.isRepository(workingDir) should equal(false)
+    //    }
+    //
+    //    it("should return true if given dir is a directory") {
+    //      val git = new Git(repositoryURL)
+    //      git.checkout(workingDir, None)
+    //      git.isRepository(workingDir) should equal(true)
+    //    }
   }
 
   private var root: File = _
   private var workingDir: File = _
-  private var repositoryURL : String = _
+  private var repositoryURL: String = _
 
   override protected def beforeEach() {
     root = new File(System.getProperty("user.dir"), "target/vcs/git")
     workingDir = new File(root, "work")
     val repository = new File(root, "repository")
     List(root, workingDir, repository).foreach(_ mkdirs)
-    
+
     run("git init ", repository)
 
     repositoryURL = repository.getAbsolutePath
@@ -47,7 +48,7 @@ class GitTest extends Spec with ShouldMatchers with BeforeAndAfterEach with Shel
     delete(root)
   }
 
-  private def makeNewCommit(repoDir:String) {
+  private def makeNewCommit(repoDir: String) {
     new File(repoDir, String.valueOf(System.currentTimeMillis) + ".txt").createNewFile
     run("git add --all")
     run("git commit -m 'added_new_file'")
