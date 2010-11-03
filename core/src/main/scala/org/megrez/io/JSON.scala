c7d79ee288a40a243dfd312ce89e01408d669879
Vincent Xu <vincent.hao.xu@gmail.com>
2010-11-03 19:42:26 +0800

    seralization for job assignment

--- core/src/main/scala/org/megrez/io/JSON.scala
+++ core/src/main/scala/org/megrez/io/JSON.scala
@@ -3,21 +3,22 @@ package org.megrez.io
 import org.megrez.vcs.Subversion
 import collection.mutable.HashMap
 import org.megrez._
+import java.util.UUID
 
 trait JSON[T] extends Reader[T, Any]
 
 trait PluggableJSON[T] extends JSON[T] {
-  private val factories = HashMap[String, Any => T]()
+  private val parser = HashMap[String, Any => T]()
 
   def register(jsonType: String, factory: Any => T) {
-    factories.put(jsonType, factory)
+    parser.put(jsonType, factory)
   }
 
   def read(json: Any) = {
     json match {
       case content: Map[Any, Any] =>
         content("type") match {
-          case jsonType: String => factories(jsonType)(json)
+          case jsonType: String => parser(jsonType)(json)
           case _ => throw new Exception()
         }
       case _ => throw new Exception()
@@ -28,7 +29,22 @@ trait PluggableJSON[T] extends JSON[T] {
 object JSON {
   def read[T](json: Any)(implicit resource: JSON[T]): T = resource.read(json)
 
-  implicit object MaterialJson extends PluggableJSON[Material]
+  implicit object MaterialJson extends PluggableJSON[Material] {
+    private val workSetParsers = HashMap[String, Any => Option[Any]]()
+
+    def registerWorkSet(jsonType: String, factory: Any => Option[Any]) {
+      workSetParsers.put(jsonType, factory)
+    }
+
+    def readWorkSet(jsonType: String, json: Any) = json match {
+      case content: Map[Any, Any] =>
+        workSetParsers.get(jsonType) match {
+          case Some(parser) => parser(json)
+          case _ => None
+        }
+      case _ => throw new Exception()
+    }
+  }
 
   implicit object PipelineJson extends JSON[Pipeline] {
     def read(json: Any) = {
@@ -58,4 +74,39 @@ object JSON {
       case _ => throw new Exception()
     }
     )
+
+  MaterialJson.registerWorkSet("svn", json =>
+    json match {
+      case content: Map[Any, Any] =>
+        content("revision") match {
+          case revision: String => Some(Integer.parseInt(revision))
+          case _ => throw new Exception()
+        }
+      case _ => throw new Exception()
+    }
+    )
+
+  implicit object JobAssignmentJson extends JSON[JobAssignment] {
+    def read(json: Any) = {
+      json match {
+        case content: Map[Any, Any] =>
+          val build = content("build") match {
+            case uuid: String => UUID.fromString(uuid)
+            case _ => throw new Exception()
+          }
+          val materials = content("materials") match {
+            case materials: List[Map[Any, Map[Any, Any]]] =>
+              materials.map(material =>
+                JSON.read[Material](material("material")) -> (material("material")("type") match {
+                  case materialType: String =>
+                    MaterialJson.readWorkSet(materialType, material("workset"))
+                  case _ => throw new Exception()
+                })).toMap
+            case _ => throw new Exception()
+          }
+          JobAssignment(build, materials, null)
+        case _ => throw new Exception()
+      }
+    }
+  }
 }
