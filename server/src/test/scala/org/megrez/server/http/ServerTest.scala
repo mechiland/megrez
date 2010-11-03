5b6654f7888c481c21eb271b942f20ce1b468504
JackyLi <twer@xiaodao.(none)>
2010-11-03 19:03:27 +0800

    uncomment some test methods

--- server/src/test/scala/org/megrez/server/http/ServerTest.scala
+++ server/src/test/scala/org/megrez/server/http/ServerTest.scala
@@ -17,68 +17,68 @@ import Method._
 import Route._
 
 class ServerTest extends Spec with ShouldMatchers with BeforeAndAfterEach with MockitoSugar {
-//  describe("HTTP Server") {
-//    it("should delegate to handler if path matched") {
-//      val event = mock[MessageEvent]
-//      when(event.getMessage).thenReturn(new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "/agent"), Array[Any]())
-//
-//      val server = new Server(
-//        path("/agent") -> self
-//      )
-//
-//      server.messageReceived(context, event)
-//
-//      receiveWithin(1000) {
-//        case _ : Request =>
-//        case TIMEOUT => fail
-//        case _ => fail
-//      }
-//    }
-//
-//    it("should delegate to handler if GET request path matched") {
-//      val event = mock[MessageEvent]
-//      when(event.getMessage).thenReturn(new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "/agent"), Array[Any]())
-//
-//      val server = new Server(
-//        get("/agent") -> self
-//      )
-//
-//      server.messageReceived(context, event)
-//
-//      receiveWithin(1000) {
-//        case request : Request => {
-//			request.method should be === GET
-//			request.uri should be === "/agent"
-//		}
-//        case TIMEOUT => fail
-//        case _ => fail
-//      }
-//    }
-//
-//	it("should delegate to handler if POST request path matched") {
-//      val event = mock[MessageEvent]
-//	  var httpRequest = new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.POST, "/pipeline")
-//	  httpRequest.setContent(ChannelBuffers.copiedBuffer("testContent", CharsetUtil.UTF_8));
-//      when(event.getMessage).thenReturn(httpRequest, Array[Any]())
-//
-//      val server = new Server(
-//        post("/pipeline") -> self
-//      )
-//
-//      server.messageReceived(context, event)
-//
-//      receiveWithin(1000) {
-//        case request : Request => {
-//			request.method should be === POST
-//			request.uri should be === "/pipeline"
-//			request.content should be === "testContent"
-//		}
-//        case TIMEOUT => fail
-//        case _ => fail
-//      }
-//    }
-//
-//  }
+  describe("HTTP Server") {
+    it("should delegate to handler if path matched") {
+      val event = mock[MessageEvent]
+      when(event.getMessage).thenReturn(new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "/agent"), Array[Any]())
+
+      val server = new Server(
+        path("/agent") -> self
+        )
+
+      server.messageReceived(context, event)
+
+      receiveWithin(1000) {
+        case _: Request =>
+        case TIMEOUT => fail
+        case _ => fail
+      }
+    }
+
+    it("should delegate to handler if GET request path matched") {
+      val event = mock[MessageEvent]
+      when(event.getMessage).thenReturn(new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "/agent"), Array[Any]())
+
+      val server = new Server(
+        get("/agent") -> self
+        )
+
+      server.messageReceived(context, event)
+
+      receiveWithin(1000) {
+        case request: Request => {
+          request.method should be === GET
+          request.uri should be === "/agent"
+        }
+        case TIMEOUT => fail
+        case _ => fail
+      }
+    }
+
+    it("should delegate to handler if POST request path matched") {
+      val event = mock[MessageEvent]
+      var httpRequest = new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.POST, "/pipeline")
+      httpRequest.setContent(ChannelBuffers.copiedBuffer("testContent", CharsetUtil.UTF_8));
+      when(event.getMessage).thenReturn(httpRequest, Array[Any]())
+
+      val server = new Server(
+        post("/pipeline") -> self
+        )
+
+      server.messageReceived(context, event)
+
+      receiveWithin(1000) {
+        case request: Request => {
+          request.method should be === POST
+          request.uri should be === "/pipeline"
+          request.content should be === "testContent"
+        }
+        case TIMEOUT => fail
+        case _ => fail
+      }
+    }
+
+  }
 
   var context: ChannelHandlerContext = _
   var pipeline: ChannelPipeline = _
