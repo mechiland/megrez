745ff81808d9133c6dbad8eb60ddf0eb6f96c052
andy <andyhu1007@gmail.com>
2010-11-09 19:35:54 +0800

    zbhu -#N/A - implement commandline task

--- core/src/test/scala/org/megrez/task/CommandLineTaskTest.scala
+++ core/src/test/scala/org/megrez/task/CommandLineTaskTest.scala
@@ -2,12 +2,28 @@ package org.megrez.task
 
 import org.scalatest.Spec
 import org.scalatest.matchers.ShouldMatchers
-import io.Source
-import java.io.{InputStreamReader, BufferedReader, InputStream}
+import java.io.File
 
 class CommandLineTaskTest extends Spec with ShouldMatchers {
   describe("Command line task") {
-    it("should ") {
+    it("should not throw exception if command execution succeed") {
+      val commandLineTask = new CommandLineTask("echo")
+      try {
+        commandLineTask.execute(new File("."))
+      }catch {
+        case e: Exception => fail
+      }
     }
+
+    it("should throw exception if command execution failed") {
+      val commandLineTask = new CommandLineTask("unknow-command")
+      try {
+        commandLineTask.execute(new File("."))
+        fail
+      } catch {
+        case e: Exception =>
+      }
+    }
+
   }
 }
