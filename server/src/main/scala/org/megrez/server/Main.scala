410a1434119ba478cacb71269432f288edcf7798
Vincent Xu <vincent.hao.xu@gmail.com>
2010-11-12 19:33:02 +0800

    http front for post pipeline

--- server/src/main/scala/org/megrez/server/Main.scala
+++ server/src/main/scala/org/megrez/server/Main.scala
@@ -2,21 +2,22 @@ package org.megrez.server
 
 import http.Route._
 import actors.Actor
-import http.{AgentWebSocketHandler, Server}
+import http.{Controllers, AgentWebSocketHandler, Server}
 import org.jboss.netty.channel.Channel
 import org.megrez.util.Logging
 
 object Main extends Logging {
   private var server: Server = null
-  private var megrez: Megrez = new Megrez
+  private val megrez = new Megrez
+  private val controllers = new Controllers(megrez)
 
   def start(port: Int) {
     def agent(channel: Channel, actor: Actor) = new AgentWebSocketHandler(channel, actor)
 
     if (server == null) {
       server = new Server(
-        websocket("/agent", agent) -> megrez.agentManager
-        )
+        websocket("/agent", agent) -> megrez.agentManager,
+        post("/pipelines") -> controllers.pipeline)
       server.start(port)
     }
   }
