c15c08550de69c7e71efdd910bcc8cfd73aaa63e
JackyLi <twer@xiaodao.(none)>
2010-12-03 08:45:04 +0800

    should manually trigger build when material has no previous changes

--- server/src/main/scala/org/megrez/server/core/trigger/OnChanges.scala
+++ server/src/main/scala/org/megrez/server/core/trigger/OnChanges.scala
@@ -3,19 +3,29 @@ package org.megrez.server.core.trigger
 import actors.Actor
 import actors.Actor._
 import org.megrez.server.trigger.Trigger
-import org.megrez.server.model.{Material, Pipeline}
 import java.io.File
 import java.util.TimerTask
 import org.megrez.Stop
 import org.megrez.server.core.TriggerBuild
+import org.megrez.server.model.{Change, Material, Pipeline}
+import org.megrez.server.model.vcs.Subversion
 
-class OnChanges(val pipeline: Pipeline, val workingDir: File, val scheduler: Actor) extends Trigger  {
+class OnChanges(val pipeline: Pipeline, val workingDir: File, val scheduler: Actor) extends Trigger {
   private val buildTrigger = actor {
     loop {
       react {
         case Trigger.Execute =>
-          scheduler ! TriggerBuild(pipeline, pipeline.materials.map { _.getChange(workingDir).get })
+          scheduler ! TriggerBuild(pipeline, pipeline.materials.map {_.getChange(workingDir).get})
+        case Trigger.ExecuteOnce =>
+          scheduler ! TriggerBuild(pipeline, pipeline.materials.map {material => getLastChange(material)})
       }
+    }
+  }
+
+  private def getLastChange(material: Material): Change = {
+    Option(material.lastChange()) match {
+      case None => material.getChange(workingDir).get
+      case _ => null
     }
   }
 
--- server/src/test/scala/org/megrez/server/core/trigger/OnChangesTest.scala
+++ server/src/test/scala/org/megrez/server/core/trigger/OnChangesTest.scala
@@ -2,7 +2,6 @@ package org.megrez.server.core.trigger
 
 import actors.Actor._
 import org.megrez.server.{IoSupport, Neo4JSupport}
-import org.scalatest.{Spec, BeforeAndAfterAll}
 import org.scalatest.matchers.ShouldMatchers
 import org.megrez.server.model._
 import data.{Plugin, Graph}
@@ -13,13 +12,38 @@ import actors.TIMEOUT
 import java.io.File
 import org.neo4j.graphdb.Node
 import org.megrez.server.core.TriggerBuild
+import org.scalatest.{BeforeAndAfterEach, Spec, BeforeAndAfterAll}
 
-class OnChangesTest extends Spec with ShouldMatchers with BeforeAndAfterAll with IoSupport with Neo4JSupport {
+class OnChangesTest extends Spec with ShouldMatchers with BeforeAndAfterAll with BeforeAndAfterEach with IoSupport with Neo4JSupport {
   describe("OnChanges Trigger") {
     it("should trigger build with changes") {
       val trigger = new OnChanges(pipeline, workingDir, self)
       trigger.start
-      
+
+      receiveWithin(1000) {
+        case TriggerBuild(triggeredPipeline: Pipeline, changes: Set[Change]) =>
+          triggeredPipeline should equal(pipeline)
+          changes should have size (1)
+          changes.head match {
+            case r: Subversion.Revision =>
+              r.revision should equal(42)
+              r.material should equal(material)
+            case _ => fail
+          }
+          trigger.stop
+        case TIMEOUT =>
+          trigger.stop
+          fail
+        case _ =>
+          trigger.stop
+          fail
+      }
+    }
+
+    it("should manually trigger build with no previous changes") {
+      val trigger = new OnChanges(pipeline, workingDir, self)
+      trigger.startTrigger ! Trigger.ExecuteOnce
+
       receiveWithin(1000) {
         case TriggerBuild(triggeredPipeline: Pipeline, changes: Set[Change]) =>
           triggeredPipeline should equal(pipeline)
@@ -41,16 +65,11 @@ class OnChangesTest extends Spec with ShouldMatchers with BeforeAndAfterAll with
     }
   }
 
-  var material : Material= null
-  var pipeline : Pipeline= null
+  var material: Material = null
+  var pipeline: Pipeline = null
   val workingDir = new File(System.getProperty("user.dir"))
 
-  override def beforeAll() {
-    Neo4J.start
-    Graph.of(neo).consistOf(Task, Job, Stage, ChangeSource, Material, Pipeline, Build, StageExecution, JobExecution)
-    Graph.consistOf(CommandLine, Ant, Subversion, Subversion.Revision)
-    Graph.consistOf(MockChangeSource)
-
+  override def beforeEach() {
     val changeSource = ChangeSource(Map("type" -> "mock"))
     material = Material(Map("source" -> changeSource))
     val author = Job(Map("name" -> "author", "tasks" -> List()))
@@ -59,6 +78,13 @@ class OnChangesTest extends Spec with ShouldMatchers with BeforeAndAfterAll with
     val ut = Stage(Map("name" -> "UT", "jobs" -> List(author, publish)))
     val packageStage = Stage(Map("name" -> "package", "jobs" -> List(packageJob)))
     pipeline = Pipeline(Map("name" -> "WGSN-bundles", "materials" -> List(material), "stages" -> List(ut, packageStage)))
+  }
+
+  override def beforeAll() {
+    Neo4J.start
+    Graph.of(neo).consistOf(Task, Job, Stage, ChangeSource, Material, Pipeline, Build, StageExecution, JobExecution)
+    Graph.consistOf(CommandLine, Ant, Subversion, Subversion.Revision)
+    Graph.consistOf(MockChangeSource)
   }
 
   override def afterAll() {
