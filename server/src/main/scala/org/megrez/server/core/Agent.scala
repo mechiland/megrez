08cb9695b89377b4cd7a5a3147f1e6d5b27a95c2
ynjia <puck984@gmail.com>
2010-12-08 23:49:59 +0800

    [nana] add resouce check for agent

--- server/src/main/scala/org/megrez/server/core/Agent.scala
+++ server/src/main/scala/org/megrez/server/core/Agent.scala
@@ -1,23 +1,31 @@
 package org.megrez.server.core
 
 import actors.Actor
-import org.megrez.server.model.{JobExecution, Build}
 import org.megrez.{JobAssignment, Stop}
 import org.megrez.util.JSON
+import collection.mutable.HashSet
+import org.megrez.server.model.{Job, JobExecution, Build}
 
 class Agent(val model: org.megrez.server.model.Agent, handler: AgentHandler, dispatcher: Actor) extends Actor {
   def act {
     loop {
       react {
-        case Pair(build: Build, execution: JobExecution) =>
-          val assignment = JobAssignment(build.id.toInt, build.pipeline.name, build.changes.map(change => (change.material.changeSource.toChangeSource, change.material.destination) -> change.toChange).toMap, execution.job.tasks.map(_.toTask).toList)
-          handler.send(JSON.write(assignment))
-          reply(AgentToDispatcher.Confirm)
+        case Pair(build: Build, jobExecution: JobExecution) =>
+          if (checkResource(jobExecution.job)) {
+            val assignment = JobAssignment(build.id.toInt, build.pipeline.name, build.changes.map(change => (change.material.changeSource.toChangeSource, change.material.destination) -> change.toChange).toMap, jobExecution.job.tasks.map(_.toTask).toList)
+            handler.send(JSON.write(assignment))
+            reply(AgentToDispatcher.Confirm)
+          }
+          else {
+            reply(AgentToDispatcher.Reject)
+          }
         case Stop => exit
         case _ =>
       }
     }
   }
+
+  private def checkResource(job: Job) = job.resources.forall(model.resources().contains(_))
   start
 }
 
